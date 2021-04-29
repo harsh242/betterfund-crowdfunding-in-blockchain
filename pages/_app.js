9d677b2216dd8e880a98f8f0c8280940e3f9006c
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-29 23:40:42 +0530

    :heavy_plus_sign: add chakra ui for styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
