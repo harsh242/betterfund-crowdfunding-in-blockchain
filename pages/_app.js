ed8796060ee27f66a7bfa22689955cd84c62b100
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-05-05 22:50:43 +0530

    :lipstick: add custom font- space grotesk

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Container } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
+
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       {" "}
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <NavBar />
         <Component {...pageProps} />
         <Footer />
