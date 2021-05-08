bd8ac728731865a06bcc9bd151480dd01c8cadeb
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-05-08 14:49:12 +0530

    :construction: v3 smart contract

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x30959f482d4E036bC43e1C1dC317222c47f20aFa"
+  "0xc77d97694eD780E1A22ce69fD50a5841341C6A67"
 );
 
 export default instance;
